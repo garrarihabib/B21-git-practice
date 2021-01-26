321a647ed88258879f3bd4202d3f29d56d97c81b
habib <74197575+garrarihabib@users.noreply.github.com>
2021-01-26 15:16:14 -0500

    movie1 updated Hello.java

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Hello B21");
         System.out.println("Second Commit");
     }
 
